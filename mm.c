267fd248954421423a5330472dc18ef65a1e6bcc
lulardou <lulostupnik2003@gmail.com>
2024-09-23 21:58:44 -0300

    format

--- mm.c
+++ mm.c
@@ -5,38 +5,38 @@ int size;
 int current;
 void *free_ptrs[BLOCK_COUNT];
 
-void my_mm_init(void *p, int s)
+void my_mm_init ( void *p, int s )
 {
-    start = p;
-    size = s;
+	start = p;
+	size = s;
 
-    for (int i = 0; i < BLOCK_COUNT; i++)
-    {
-        free_ptrs[i] = start + i * BLOCK_SIZE;
-    }
-    current = 0;
+	for ( int i = 0; i < BLOCK_COUNT; i++ ) {
+		free_ptrs[i] = start + i * BLOCK_SIZE;
+	}
+	current = 0;
 }
 
-void *my_malloc(int size)
+void *my_malloc ( int size )
 {
-    if (size > BLOCK_SIZE || current >= BLOCK_COUNT)
-    {
-        return NULL;
-    }
-    return free_ptrs[current++];
+	if ( size > BLOCK_SIZE || current >= BLOCK_COUNT ) {
+		return NULL;
+	}
+	return free_ptrs[current++];
 }
 
-void my_free(void *p)
+void my_free ( void *p )
 {
-    free_ptrs[--current] = p;
+	free_ptrs[--current] = p;
 }
 
 int main()
 {
-    char mem[BLOCK_COUNT * BLOCK_SIZE];
-    my_mm_init((void *)mem, BLOCK_COUNT * BLOCK_SIZE);
-    printf("Corriendo el test...\nCortar con Ctrl+C\n");
-    test_mm(1, (char *[]){MEM_SIZE_STR});
+	char mem[BLOCK_COUNT * BLOCK_SIZE];
+	my_mm_init ( ( void * ) mem, BLOCK_COUNT * BLOCK_SIZE );
+	printf ( "Corriendo el test...\nCortar con Ctrl+C\n" );
+	test_mm ( 1, ( char *[] ) {
+		MEM_SIZE_STR
+	} );
 
-    return 0;
+	return 0;
 }
